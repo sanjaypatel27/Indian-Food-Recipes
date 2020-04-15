7fb3f2dfaa1210ce989cd0b854af3b149930f42d
Sanjay Patel <sanjay@smooth.tech>
2020-04-14 20:11:30 -0400

    #3 create new blank activity
    delete bottom navigation activity and its data

---
+++ app/src/main/java/com/diyainfotech/indiafoodreceipes/ui/home/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.diyainfotech.indiafoodreceipes.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.diyainfotech.indiafoodreceipes.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
